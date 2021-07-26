385ca860312ef5a1fb8674d2f27cb86d45234449
balazs.saros <balazs.saros@gmail.com>
2021-07-26 22:28:13 +0200

    working prototype

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import {
   TextField,
   IconButton,
@@ -9,70 +9,75 @@ import {
   ShareRounded,
 } from '@material-ui/icons'
 import { Picker } from 'emoji-mart';
-import html2canvas from 'html2canvas';
+import { toPng } from 'html-to-image';
 
 import 'emoji-mart/css/emoji-mart.css'
 import './App.scss';
 
 const App = () => {
-  const [primaryText, setPrimaryText] = useState('ez egy plakát?');
-  const [secondaryText, setSecondarytext] = useState('az ám!');
-  const [emoji, setEmoji] = useState('yawning_face');
+  const canvasRef = useRef(null);
+  const [primaryText, setPrimaryText] = useState('ön is szeretne saját plakátot?');
+  const [secondaryText, setSecondarytext] = useState('készítsen egyet!');
+  const [emoji, setEmoji] = useState('1f9d1-200d-1f3a8');
 
   const decorateText = (text) => {
-    const firstPart = text.split(' ').splice(0, 2).join(' ').toUpperCase();
-    const secondPart = text.split(' ').splice(2).join(' ').toUpperCase();
+    const splitText = text.split(' ');
+    const splitIndex = splitText.length > 2 ? 2 : 1;
+    const firstPart = text.split(' ').splice(0, splitIndex).join(' ').toUpperCase();
+    const secondPart = text.split(' ').splice(splitIndex).join(' ').toUpperCase();
     return (
       <>
-        <span className="Canvas__subtitle--redPart">{firstPart}</span>
-        { secondPart && <span className="Canvas__subtitle--greenPart">&nbsp;{secondPart}</span> }
+        <div className="Canvas__subtitle--redPart">{firstPart}</div>
+        { secondPart && <div className="Canvas__subtitle--greenPart">&nbsp;{secondPart}</div> }
       </>
     );
   };
 
   const getImg = () => {
-    html2canvas(document.querySelector("#capture")).then(canvas => {
-      const link = document.createElement('a');
-      link.download = 'plakat-test.png';
-      link.href = canvas.toDataURL()
-      link.click();
+    toPng(canvasRef.current, { pixelRatio: 1 }).then((dataUrl) => {
+      const link = document.createElement('a')
+      link.download = 'my-image-name.png'
+      link.href = dataUrl
+      link.click()
     });
   };
 
   return (
-    <div className="container">
-      <div className="Toolbar">
-        {/* <IconButton> <Rotate90DegreesCcwRounded /> </IconButton> */}
-        <IconButton onClick={getImg}>
-          <GetAppRounded />
-        </IconButton>
-        {/* <IconButton> <ShareRounded /> </IconButton> */}
-      </div>
-      <div className="Canvas" id="capture">
-        <div className="Canvas__title">{primaryText.toUpperCase()}</div>
-        <img src={`/emoji-images/160x160/${emoji.toLowerCase().replace(' ', '_')}.png`} alt="" />
-        {secondaryText && (
-          <div className="Canvas__subtitle">
-            {decorateText(secondaryText)}
-          </div>
-        )}
-      </div>
-      <div className="Fields">
-        <TextField
-          className="field"
-          label="egyik"
-          variant="outlined"
-          value={primaryText}
-          onChange={(e) => setPrimaryText(e.target.value)}
-        />
-        <TextField
-          className="field"
-          label="másik"
-          variant="outlined"
-          value={secondaryText}
-          onChange={(e) => setSecondarytext(e.target.value)}
-        />
-        <Picker onSelect={(emoji) => setEmoji(emoji.name)} style={{width: '100%'}} exclude={['recent']} showSkinTones={false} showPreview={false}/>
+    <div className="outer-container">
+      <div className="container">
+        <div className="Toolbar">
+          {/* <IconButton> <Rotate90DegreesCcwRounded /> </IconButton> */}
+          <IconButton onClick={getImg}>
+            <GetAppRounded />
+          </IconButton>
+          {/* <IconButton> <ShareRounded /> </IconButton> */}
+        </div>
+        <div className="Canvas" id="capture" ref={canvasRef}>
+          <div className="Canvas__title">{primaryText.toUpperCase()}</div>
+          <img src={`/img-apple-160/${emoji}.png`} alt="" />
+          {secondaryText && (
+            <div className="Canvas__subtitle">
+              {decorateText(secondaryText)}
+            </div>
+          )}
+        </div>
+        <div className="Fields">
+          <TextField
+            className="field"
+            label="egyik"
+            variant="outlined"
+            value={primaryText}
+            onChange={(e) => setPrimaryText(e.target.value)}
+          />
+          <TextField
+            className="field"
+            label="másik"
+            variant="outlined"
+            value={secondaryText}
+            onChange={(e) => setSecondarytext(e.target.value)}
+          />
+          <Picker onSelect={(emoji) => setEmoji(emoji.unified)} style={{width: '100%'}} exclude={['recent']} showSkinTones={false} showPreview={false}/>
+        </div>
       </div>
     </div>
   );
