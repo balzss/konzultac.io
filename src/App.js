058db4cf1d2a46c79d54b8cff2305edebefbbf49
balazs.saros <balazs.saros@gmail.com>
2021-07-27 18:05:29 +0200

    sharing is caring, readme

--- src/App.js
+++ src/App.js
@@ -14,11 +14,17 @@ import { toPng } from 'html-to-image';
 import 'emoji-mart/css/emoji-mart.css'
 import './App.scss';
 
+const defaults = {
+  primaryText: 'Ön is szeretne saját plakátot?',
+  secondaryText: 'Készítsen egyet!',
+  emoji: '1f9d1-200d-1f3a8', // artist emoji
+};
+
 const App = () => {
   const canvasRef = useRef(null);
-  const [primaryText, setPrimaryText] = useState('ön is szeretne saját plakátot?');
-  const [secondaryText, setSecondarytext] = useState('készítsen egyet!');
-  const [emoji, setEmoji] = useState('1f9d1-200d-1f3a8');
+  const [primaryText, setPrimaryText] = useState(defaults.primaryText);
+  const [secondaryText, setSecondarytext] = useState(defaults.secondaryText);
+  const [emoji, setEmoji] = useState(defaults.emoji);
 
   const decorateText = (text) => {
     const splitText = text.split(' ');
@@ -33,7 +39,7 @@ const App = () => {
     );
   };
 
-  const getImg = () => {
+  const downloadImage = () => {
     toPng(canvasRef.current, { pixelRatio: 1 }).then((dataUrl) => {
       const link = document.createElement('a');
       link.download = `${primaryText.split(' ').splice(0, 3).join(' ')} (${new Date(Date.now()).toISOString().substring(0, 10)})`;
@@ -42,15 +48,27 @@ const App = () => {
     });
   };
 
+  const shareImage = () => {
+    toPng(canvasRef.current, { pixelRatio: 1 }).then(async (dataUrl) => {
+      const blob = await (await fetch(dataUrl)).blob();
+      const files = [new File([blob], 'plakat.png', { type: blob.type, lastModified: new Date().getTime() })];
+      navigator.share({ files });
+    });
+  };
+
   return (
     <div className="outer-container">
       <div className="container">
         <div className="Toolbar">
           {/* <IconButton> <Rotate90DegreesCcwRounded /> </IconButton> */}
-          <IconButton onClick={getImg}>
+          <IconButton onClick={downloadImage}>
             <GetAppRounded />
           </IconButton>
-          {/* <IconButton> <ShareRounded /> </IconButton> */}
+          {navigator.share && (
+            <IconButton onClick={shareImage}>
+              <ShareRounded />
+            </IconButton>
+          )}
         </div>
         <div className="Canvas" id="capture" ref={canvasRef}>
           <div className="Canvas__title">{primaryText.toUpperCase()}</div>
